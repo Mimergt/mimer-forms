232b7ca26b2047487c6090c8c4e2e0d77c532d09
MimerGT <mimer@epic.gt>
2025-08-01 16:38:23 -0600

    Redirigir según redirect_url de la API o a la página local de gracias

--- includes/forms-api.php
+++ includes/forms-api.php
@@ -53,5 +53,21 @@ $data = [
         }
 
         file_put_contents(plugin_dir_path(__FILE__) . '/../log.txt', $log, FILE_APPEND);
+
+        // Procesar redirección
+        if (!is_wp_error($response)) {
+            $body = wp_remote_retrieve_body($response);
+            $json = json_decode($body, true);
+            if (isset($json['redirect_url']) && !empty($json['redirect_url'])) {
+                wp_redirect($json['redirect_url']);
+                exit;
+            } else {
+                wp_redirect('https://injuryresolve.com/dp-thankyou/');
+                exit;
+            }
+        } else {
+            wp_redirect('https://injuryresolve.com/dp-thankyou/');
+            exit;
+        }
     }
 }
